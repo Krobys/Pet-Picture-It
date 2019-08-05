bf7482fa72ec82195a484404b4e4be515d3c4c13
Krobys <krivonos.andrew@gmail.com>
2019-08-05 17:25:09 +0300

    remove loading from UI + Add AsyncTask instead Runnable +

--- app_standart_java/src/main/java/com/akrivonos/app_standart_java/models/Photo.java
+++ app_standart_java/src/main/java/com/akrivonos/app_standart_java/models/Photo.java
@@ -1,6 +1,9 @@
 package com.akrivonos.app_standart_java.models;
 
-public class Photo {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Photo implements Parcelable {
 
     private String id;
 
@@ -9,6 +12,29 @@ public class Photo {
     private String server;
 
     private String farm;
+
+    public static final Creator<Photo> CREATOR = new Creator<Photo>() {
+        @Override
+        public Photo createFromParcel(Parcel in) {
+            return new Photo(in);
+        }
+
+        @Override
+        public Photo[] newArray(int size) {
+            return new Photo[size];
+        }
+    };
+
+    private Photo(Parcel in) {
+        id = in.readString();
+        secret = in.readString();
+        server = in.readString();
+        farm = in.readString();
+    }
+
+    public Photo() {
+
+    }
 
     public String getId() {
         return id;
@@ -40,6 +66,19 @@ public class Photo {
 
     public void setFarm(String farm) {
         this.farm = farm;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(id);
+        dest.writeString(secret);
+        dest.writeString(server);
+        dest.writeString(farm);
     }
 }
 
--- app_standart_java/src/main/java/com/akrivonos/app_standart_java/services/PicturesDownloadService.java
+++ app_standart_java/src/main/java/com/akrivonos/app_standart_java/services/PicturesDownloadService.java
@@ -1,18 +1,25 @@
 package com.akrivonos.app_standart_java.services;
 
+import android.annotation.SuppressLint;
 import android.app.Service;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.IBinder;
 
 import com.akrivonos.app_standart_java.MainActivity;
+import com.akrivonos.app_standart_java.models.Photo;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.StringReader;
 import java.net.ProtocolException;
 import java.net.URL;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.ArrayList;
 
 import javax.net.ssl.HttpsURLConnection;
 
@@ -23,17 +30,11 @@ import static com.akrivonos.app_standart_java.MainActivity.STATUS_STOP;
 
 public class PicturesDownloadService extends Service {
     public static final String STATUS = "STATUS";
-    private ExecutorService executorService;
-    private String urlDownload;
-
-    public PicturesDownloadService() {
-        executorService = Executors.newFixedThreadPool(1);
-    }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        urlDownload = buildUrlForSearchWithSearchText(intent.getStringExtra(SEARCH_TEXT));
-        executorService.execute(new RunTask(startId));
+        String urlDownload = buildUrlForSearchWithSearchText(intent.getStringExtra(SEARCH_TEXT));
+        new RunLoadingPictures().execute(urlDownload);
         return super.onStartCommand(intent, flags, startId);
     }
 
@@ -47,27 +48,64 @@ public class PicturesDownloadService extends Service {
         return "https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=" + API_KEY + "&text=" + searchText;
     }
 
-    private class RunTask implements Runnable {
-        int startId;
+    @SuppressLint("StaticFieldLeak")
+    private class RunLoadingPictures extends AsyncTask<String, Void, ArrayList<Photo>> {
 
-        RunTask(int startId) {
-            this.startId = startId;
+        @Override
+        protected void onPreExecute() {
+            sendBroadcast(new Intent(MainActivity.BROADCAST_ACTION)
+                    .putExtra(STATUS, STATUS_START));
+            super.onPreExecute();
         }
 
         @Override
-        public void run() {
-            sendBroadcast(new Intent(MainActivity.BROADCAST_ACTION)
-                    .putExtra(STATUS, STATUS_START));
-
-            String resultInformation = loadInformation();
-            sendBroadcast(new Intent(MainActivity.BROADCAST_ACTION)
-                    .putExtra(STATUS, STATUS_STOP)
-                    .putExtra(RESULT_TEXT, resultInformation));
-
-            stopSelf(startId);
+        protected ArrayList<Photo> doInBackground(String... strings) {
+            return loadInformation(strings[0]);
         }
 
-        private String loadInformation() { // Загрузка xml в String
+        @Override
+        protected void onPostExecute(ArrayList<Photo> photos) {
+            sendBroadcast(new Intent(MainActivity.BROADCAST_ACTION)
+                    .putExtra(STATUS, STATUS_STOP)
+                    .putExtra(RESULT_TEXT, photos));
+            super.onPostExecute(photos);
+        }
+
+        private ArrayList<Photo> parseXml(String xml) throws XmlPullParserException, IOException { // Парсинг фотографий в список
+            ArrayList<Photo> photos = new ArrayList<>();
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+            factory.setNamespaceAware(true);
+            XmlPullParser xpp = factory.newPullParser();
+            xpp.setInput(new StringReader(xml));
+            while (xpp.getEventType() != XmlPullParser.END_DOCUMENT) {
+                if (xpp.getEventType() == XmlPullParser.START_TAG) {
+                    if (xpp.getName().equals("photo")) {
+                        Photo photo = new Photo();
+                        for (int i = 0; i < xpp.getAttributeCount(); i++) {
+                            switch (xpp.getAttributeName(i)) {
+                                case "id":
+                                    photo.setId(xpp.getAttributeValue(i));
+                                    break;
+                                case "secret":
+                                    photo.setSecret(xpp.getAttributeValue(i));
+                                    break;
+                                case "server":
+                                    photo.setServer(xpp.getAttributeValue(i));
+                                    break;
+                                case "farm":
+                                    photo.setFarm(xpp.getAttributeValue(i));
+                                    break;
+                            }
+                        }
+                        photos.add(photo);
+                    }
+                }
+                xpp.next();
+            }
+            return photos;
+        }
+
+        private ArrayList<Photo> loadInformation(String urlDownload) { // Загрузка xml в String
             BufferedReader reader = null;
             StringBuilder buf = new StringBuilder();
             URL url;
@@ -95,7 +133,15 @@ public class PicturesDownloadService extends Service {
                     }
                 }
             }
-            return new String(buf);
+            ArrayList<Photo> photos = new ArrayList<>();
+            try {
+                photos = parseXml(new String(buf));
+            } catch (XmlPullParserException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return photos;
         }
     }
 }
